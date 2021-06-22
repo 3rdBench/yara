84ab20e9978c3fef5da9fe14ec720877d7fc5848
Benjie Lazaro <benjie.work@gmail.com>
2021-06-22 16:37:51 +0800

    Update comments

--- soc-challenge/create_rule.py
+++ soc-challenge/create_rule.py
@@ -9,23 +9,23 @@ malware_strings = intel_file.readlines()
 
 string_line_count = 0
 
-# Generate YARA header & write to file (malware.yar)
+# Write YARA header to file (malware.yar)
 rule_file.write('rule malware\n')
 rule_file.write('{\n')
 rule_file.write('	strings:\n')
 
-# Read malware string per line, generate YARA rule & write to file
+# Read malware string from source, convert into a YARA rule & write to file
 for malware_string in malware_strings:
 	string_line_count += 1
 
 	rule_file.write(f'		$foo{string_line_count} = "{malware_string.strip()}"\n')
 
-# Generate YARA condition & write to file
+# Write YARA condition to file
 rule_file.write('\n')
 rule_file.write('	condition:\n')
 rule_file.write('		all of them\n')
 rule_file.write('}')
 
-# Gracefully close opened files
+# Close opened files
 intel_file.close()
 rule_file.close()
